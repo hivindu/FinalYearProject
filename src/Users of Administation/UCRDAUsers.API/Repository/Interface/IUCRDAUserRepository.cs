fd6102ca8c4078575c2cacb2ffba39cc0933f1ee
hivindu <h.amaradeva@gmail.com>
2021-04-04 00:15:23 +0530

    Repository class method initialization

--- src/Users of Administation/UCRDAUsers.API/Repository/Interface/IUCRDAUserRepository.cs
+++ src/Users of Administation/UCRDAUsers.API/Repository/Interface/IUCRDAUserRepository.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UCRDAUsers.API.Entities;
 
 namespace UCRDAUsers.API.Repository.Interface
 {
     public interface IUCRDAUserRepository
     {
+        Task<IEnumerable<UCRDAUser>> GetUsers();
 
+        Task<UCRDAUser> GetUSer(string id);
+
+        Task<UCRDAUser> GetUserByCredentials(string nic,string password);
+
+        Task Create(UCRDAUser user);
+
+        Task<bool> Update(UCRDAUser user);
+
+        Task<bool> Delete(string id);
     }
 }
--- src/Users of Administation/UCRDAUsers.API/Repository/UCRDAUserRepository.cs
+++ src/Users of Administation/UCRDAUsers.API/Repository/UCRDAUserRepository.cs
@@ -2,12 +2,41 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UCRDAUsers.API.Entities;
 using UCRDAUsers.API.Repository.Interface;
 
 namespace UCRDAUsers.API.Repository
 {
     public class UCRDAUserRepository : IUCRDAUserRepository
     {
+        public Task<IEnumerable<UCRDAUser>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
 
+        public Task<UCRDAUser> GetUSer(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UCRDAUser> GetUserByCredentials(string nic, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Create(UCRDAUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(UCRDAUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

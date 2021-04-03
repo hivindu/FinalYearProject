202af81d4662b68249baff1e4e36e37012c2e383
hivindu <h.amaradeva@gmail.com>
2021-04-03 17:56:51 +0530

    Context classes creation

--- src/Users of Administation/UCRDAUsers.API/Data/Interface/IUCRDAUserContext.cs
+++ src/Users of Administation/UCRDAUsers.API/Data/Interface/IUCRDAUserContext.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using UCRDAUsers.API.Entities;
 
 namespace UCRDAUsers.API.Data.Interface
 {
     public interface IUCRDAUserContext
     {
+        IMongoCollection<UCRDAUser> ucrdausers {get;}
     }
 }
---
+++ src/Users of Administation/UCRDAUsers.API/Data/UCRDAUserContext.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UCRDAUsers.API.Data.Interface;
+using UCRDAUsers.API.Entities;
+
+namespace UCRDAUsers.API.Data
+{
+    public class UCRDAUserContext : IUCRDAUserContext
+    {
+        public IMongoCollection<UCRDAUser> ucrdausers { get; }
+    }
+}
